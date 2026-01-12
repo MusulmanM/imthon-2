5ec7459aaa5fe21cfceefd7f667f20b23d6fda5a
MusulmanM <mmmmmmvvv200@gmail.com>
2026-01-12 17:28:49 +0500

    birinchi commit

--- Users/models.py
+++ Users/models.py
@@ -1,26 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
 # Create your models here.
 
 
 class User(AbstractUser):
     class Role(models.TextChoices):
-        ADMIN = 'ADMIN', 'admin'
-        DENTISTS = 'DENTISTS', 'doctor'
-        CLIENT = 'CLIENT', 'client'
+        ADMIN = "ADMIN", "admin"
+        DENTISTS = "DENTISTS", "doctor"
+        CLIENT = "CLIENT", "client"
+
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=20)
-    
+
     groups = models.ManyToManyField(
-        'auth.Group',
-        related_name='custom_user_groups',
-        blank=True
+        "auth.Group", related_name="custom_user_groups", blank=True
     )
     user_permissions = models.ManyToManyField(
-        'auth.Permission',
-        related_name='custom_user_permissions',
-        blank=True
+        "auth.Permission", related_name="custom_user_permissions", blank=True
     )
-
-
-
